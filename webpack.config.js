e9397e702018baa70447546937fafcb25e5acf54
eng-Marcio <marcio37nandi@gmail.com>
2021-01-07 20:48:21 -0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const { EntryOptionPlugin } = require("webpack");
+const path = require('path')
+
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development', 
+    watch: true
+}
