b28c1aa0885bac16a6e4bf3fd85136e8cd88f354
eng-Marcio <marcio37nandi@gmail.com>
2021-01-08 10:36:23 -0300

    webpack with fast reload

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
-    mode: 'development', 
-    watch: true,
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+    mode: 'development',
     module: {
         rules: [
             {
