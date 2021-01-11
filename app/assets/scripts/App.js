b4d8ebe4fd36e39f3740cbe60a9b764ae1e9aed7
eng-Marcio <marcio37nandi@gmail.com>
2021-01-11 09:15:25 -0300

    js changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,3 +4,7 @@ import '../styles/styles.css'
 if (module.hot) {
     module.hot.accept()
 }
+
+/*lesson example code */
+console.log("Hello, my name is John doe and my favorite color is blue.");
+console.log("Hello, my name is Jane doe and my favorite color is green.");
