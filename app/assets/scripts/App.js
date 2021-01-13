75ca660d69d070870be7e4dbd64a28a7f2fba1b0
eng-Marcio <marcio37nandi@gmail.com>
2021-01-12 22:19:03 -0300

    react baseline included in the project

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,16 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 import ClientArea from './modules/ClientArea'
 
+/*
+//React related goes here
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import our code for React components
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+*/
 
 new ClientArea();
 new StickyHeader();
---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+        <div className="section-title section-title--blue">
+            <h1>This is My Amazing Component</h1>
+            <p>React is great, the sky is blue, grass is green.</p>
+        </div>
+    )
+}
+
+export default MyAmazingComponent
